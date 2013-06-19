eb88776da413efd7f6c88a3a52209134ee99495f
Dan Tello <dan.tello@gmail.com>
2013-06-18 22:09:16 -0400

    Add background pausing

---
+++ javascript/init.js
@@ -0,0 +1,16 @@
+(function() {
+
+	var pauseBg = function() {
+		document.body.setAttribute('class', '');
+	};
+
+	var animateBg = function() {
+		document.body.setAttribute('class', 'animate');
+	};
+
+	window.addEventListener('blur', pauseBg, false);
+	window.addEventListener('focus', animateBg, false);
+
+	Game.load(BlockBlaster);
+
+})();
